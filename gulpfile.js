63452e7e687e0633864a20bccdb19762fd48fd3c
Nikita Suchkov <suchkov8520@gmail.com>
2022-09-05 20:54:29 +0300

    Format: gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,16 @@
 const requireDir = require("require-dir");
 global.$ = {
-   gulp:           require("gulp"),
-   sass:           require("gulp-sass")(require("sass")),
-   app:            require("gulp-load-plugins")({ DEBUG: false}),
-   path:           require("./src/gulp/config/path"),
-   task:           requireDir("./src/gulp/tasks/", { recurse: true }),
-   browserSync:    require("browser-sync").create(),
-   compiler:       require("webpack-stream"),
+   gulp: require("gulp"),
+   sass: require("gulp-sass")(require("sass")),
+   app: require("gulp-load-plugins")({ DEBUG: false }),
+   path: require("./src/gulp/config/path"),
+   task: requireDir("./src/gulp/tasks/", { recurse: true }),
+   browserSync: require("browser-sync").create(),
+   compiler: require("webpack-stream"),
 };
 
-
 /* Запускает сборку без запуска сервера */
-exports.default =
-   $.gulp.series(
-   $.task.dev.clean.build,
-   $.task.dev.transfer.img,
-   $.task.dev.transfer.webp,
-   $.task.dev.transfer.video,
-   $.task.dev.transfer.fonts,
-   $.task.dev.transfer.js,
-   $.task.dev.transfer.vendorJs,
-   $.task.dev.html,
-   $.task.dev.styles,
-   $.task.dev.transfer.admin);
-
-
-/* Запускает сборку с запуском сервера */
-exports.dev =
-   $.gulp.series(
+exports.default = $.gulp.series(
    $.task.dev.clean.build,
    $.task.dev.transfer.img,
    $.task.dev.transfer.webp,
@@ -38,11 +21,25 @@ exports.dev =
    $.task.dev.html,
    $.task.dev.styles,
    $.task.dev.transfer.admin,
-   $.task.dev.server);
+);
+
+/* Запускает сборку с запуском сервера */
+exports.dev = $.gulp.series(
+   $.task.dev.clean.build,
+   $.task.dev.transfer.img,
+   $.task.dev.transfer.webp,
+   $.task.dev.transfer.video,
+   $.task.dev.transfer.fonts,
+   $.task.dev.transfer.js,
+   $.task.dev.transfer.vendorJs,
+   $.task.dev.html,
+   $.task.dev.styles,
+   $.task.dev.transfer.admin,
+   $.task.dev.server,
+);
 
 /* Запускает продвкшен сборку */
-exports.pub =
-   $.gulp.series(
+exports.pub = $.gulp.series(
    $.task.pub.clean,
    $.task.pub.transfer.img,
    $.task.pub.transfer.webp,
@@ -53,7 +50,8 @@ exports.pub =
    $.task.pub.htmlInclude,
    $.task.pub.html,
    $.task.pub.styles,
-   $.task.pub.transfer.admin);
+   $.task.pub.transfer.admin,
+);
 
 exports.zip = $.task.pub.zip;
 exports.server = $.task.pub.server;
